441ffe1dffd4e056af939556d102b0ebd3377843
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-10-26 02:00:44 +0900

    fix: prevent going to error route when quote click (modal)
    use location object to set the background & render modal on top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import React from "react";
-import { Route, Switch, Link, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import MainNavigation from "./components/Layout/MainNavigation";
 import MainFooter from "./components/Layout/MainFooter";
+import QuoteDetail from "./components/Quotes/QuoteDetail";
+import { useLocation } from "react-router";
+import QuoteDetailModal from "./components/Quotes/QuoteDetailModal";
 
 function App() {
+	let location = useLocation();
+	let background = location.state && location.state.background;
+
 	return (
 		<div className="App">
 			<MainNavigation />
 
-			<Switch>
+			<Switch location={background || location}>
 				<Route path="/" exact>
 					<Main />
 				</Route>
 
-				<Route path="/:quoteId" exact>
-					<div>QuoteId</div>
+				<Route path="/quotes/:quoteId">
+					<QuoteDetail />
 				</Route>
 
 				<Route path="/author/:authorId">
@@ -30,7 +36,19 @@ function App() {
 				<Route path="/user/:userId">
 					<div>UserId</div>
 				</Route>
+
+				<Route path="*">
+					<div>ERROR</div>
+				</Route>
 			</Switch>
+
+			{background && (
+				<Route path="/quotes/:quoteId">
+					<QuoteDetailModal>
+						<QuoteDetail />
+					</QuoteDetailModal>
+				</Route>	
+			)}
 
 			<MainFooter />
 		</div>
