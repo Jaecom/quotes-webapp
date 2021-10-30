a1bdc67f9efef58fe347818dc554e7ecb8dfe266
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-10-30 14:10:31 +0900

    feat: adding custom hooks - useHttp & useObserver for infinite scrolling quotes

---
+++ frontend/src/hooks/useHttp.ts
@@ -0,0 +1,49 @@
+import { useState, useCallback } from "react";
+
+interface RequestObject {
+	url: string;
+	body: string;
+	method: string;
+	headers: {
+		[header: string]: string;
+	};
+}
+
+const useHttp = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
+
+	const sendRequest = useCallback(
+		async (requestObject: RequestObject, processData: (data: any) => {}) => {
+			try {
+				const { url, body, method, headers } = requestObject;
+
+				setIsLoading(true);
+
+				const res = await fetch(url, {
+					body: body ? JSON.stringify(body) : null,
+					method: method ?? "GET",
+					headers: headers ?? { "Content-Type": "application/json" },
+				});
+
+				if (!res.ok) {
+					throw new Error("Something went wrong");
+				}
+
+				const data = await res.json();
+
+				processData(data);
+
+				setIsLoading(false);
+			} catch (e: any) {
+				setError(e.message);
+				setIsLoading(false);
+			}
+		},
+		[]
+	);
+
+	return [sendRequest, isLoading, error];
+};
+
+export default useHttp;
---
+++ frontend/src/hooks/useObserver.ts
@@ -0,0 +1,33 @@
+import { useCallback, useMemo } from "react";
+
+const useObserver = (target: React.RefObject<HTMLElement>, callback: () => {}) => {
+	const observer = useMemo(() => {
+		const ObserverCallback = (entries: any, observer: any) => {
+			for (const entry of entries) {
+				if (entry.isIntersecting) {
+					callback();
+				}
+			}
+		};
+
+		const options = {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0.1,
+		};
+
+		return new IntersectionObserver(ObserverCallback, options);
+	}, [callback]);
+
+	const attachObserver = useCallback(() => {
+		target?.current && observer.observe(target.current);
+	}, [observer, target]);
+
+	const detachObserver = useCallback(() => {
+		target?.current && observer.unobserve(target.current);
+	}, [observer, target]);
+
+	return [attachObserver, detachObserver];
+};
+
+export default useObserver;
