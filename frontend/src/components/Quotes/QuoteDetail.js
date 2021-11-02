ff10a4abc86f6eba2f36335cb36a721211a21f0c
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-11-02 15:32:31 +0900

    refractor: change quote.title prop to quote.source (broader meaning)

--- frontend/src/components/Quotes/QuoteDetail.js
+++ frontend/src/components/Quotes/QuoteDetail.js
@@ -1,29 +1,25 @@
 import classes from "./QuoteDetail.module.scss";
-import { useParams } from "react-router";
-import quotes from "../../seeds/quoteData";
 
-const QuoteDetail = () => {
-	console.log("QuoteDetailModal");
-	const { quoteId } = useParams();
-	const quote = quotes.find((element) => element.id === quoteId);
+const QuoteDetail = (props) => {
+	const quote = props.quote;
 
 	return (
 		<div className={classes.background}>
 			<div className={classes.content}>
 				<div className={classes.info}>
-					<h3 className="heading-4--bold">{quote.title}</h3>
+					<h3 className="heading-4--bold">{quote.source}</h3>
 					<h3 className="heading-4">{quote.author}</h3>
 				</div>
 
 				<div className={classes.key}>
 					<p className={`paragraph--big ${classes["quotation-mark"]}`}>&ldquo;</p>
 					<h2 className="paragraph--big">{quote.keywords}</h2>
-					<p className="paragraph--medium">{quote.exlcudeKeywords}</p>
+					<p className="paragraph--medium">{quote.excludeKeywords}</p>
 				</div>
 
 				<div className={classes["image-container"]}>
-					<img className={classes.image} src={quote.image} />
-					<h2 className={`${classes.title} heading-2--cap`}>{quote.title}</h2>
+					<img className={classes.image} src={quote.image} alt={"quote"} />
+					<h2 className={`${classes.source} heading-2--cap`}>{quote.source}</h2>
 				</div>
 
 				{/* <div className={classes.break}></div> */}
