4188fe4ed155206880fbdea0f289c5e9956ed3c6
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-11-07 22:13:24 +0900

    fix: scroll to top in modal view when another link clicked

--- frontend/src/components/Quotes/QuoteDetail/QuoteDetailModal.js
+++ frontend/src/components/Quotes/QuoteDetail/QuoteDetailModal.js
@@ -1,15 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useHistory } from "react-router";
 import Modal from "../../UI/Modal";
 import classes from "./QuoteDetailModal.module.scss";
 
 const QuoteDetailModal = (props) => {
 	const history = useHistory();
+	const quoteModalRef = useRef();
+	console.log("QuoteDetailModal");
 
 	useEffect(() => {
 		document.body.style.overflow = "hidden";
 		return () => (document.body.style.overflow = "unset");
 	});
+
+	useEffect(() => {
+		const unlisten = history.listen(() => {
+			quoteModalRef.current.scrollTo(0, 0);
+		});
+
+		return () => {
+			unlisten();
+		};
+	}, [history]);
 
 	const modalCloseHander = () => {
 		history.push("/");
@@ -17,7 +29,9 @@ const QuoteDetailModal = (props) => {
 
 	return (
 		<Modal onClose={modalCloseHander}>
-			<div className={classes.background}>{props.children}</div>
+			<div className={classes.background} ref={quoteModalRef}>
+				{props.children}
+			</div>
 		</Modal>
 	);
 };
