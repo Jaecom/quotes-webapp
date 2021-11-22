337b881db1f21276202126e3fbe8b65a203b2932
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-11-22 23:59:49 +0900

    feat: add login form

--- frontend/src/components/Auth/LoginForm.js
+++ frontend/src/components/Auth/LoginForm.js
@@ -1,17 +1,44 @@
 import classes from "./AuthForm.module.scss";
 import Input from "./Input";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
+import useHttp from "../../hooks/useHttp";
 
 const LoginForm = () => {
+	const formRef = useRef();
+	const [sendRequest, isLoading, error] = useHttp();
+
+	const submitHandler = async (event) => {
+		event.preventDefault();
+
+		console.log(formRef.current);
+		const formData = new FormData(formRef.current);
+		const formObject = Object.fromEntries(formData);
+
+		sendRequest(
+			{
+				url: "http://localhost:5000/api/users/login",
+				method: "POST",
+				body: new URLSearchParams(formObject),
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			},
+			(data) => {
+				console.log(data);
+			}
+		);
+	};
+
 	return (
-		<form>
+		<form className={classes.form} onSubmit={submitHandler} ref={formRef}>
 			<h2 className={`${classes.heading}`}>Login</h2>
 			<p className={classes.redirect}>
 				Don't have an account?&nbsp;
 				<Link to="/signup">Sign Up</Link>
 			</p>
-			<Input id="email" attribute={{ type: "email" }} label="Email" />
-			<Input id="password" attribute={{ type: "password" }} label="Password" />
+			<Input id="email" attribute={{ type: "email" }} label="Email" name="email" />
+			<Input id="password" attribute={{ type: "password" }} label="Password" name="password" />
 			<button type="submit">Login</button>
 		</form>
 	);
