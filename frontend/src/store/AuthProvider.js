cb6987fd30d1a9c14e7d702804dc04e64e0e29f6
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-11-25 15:18:46 +0900

    feat: addlogin, logout, and autologout logic

--- frontend/src/store/AuthProvider.js
+++ frontend/src/store/AuthProvider.js
@@ -1,30 +1,56 @@
 import AuthContext from "./auth-context";
 import { useEffect, useState } from "react";
-
+import { useHistory } from "react-router-dom";
 const LOCAL_TOKEN_KEY = "token";
 const LOCAL_TOKEN_EXPIRATION_KEY = "expiration";
+let logoutTimer;
 
 //initially retrieve token
 const retrieveToken = () => {
-		
-}
+	const token = localStorage.getItem(LOCAL_TOKEN_KEY);
+	const expiration = localStorage.getItem(LOCAL_TOKEN_EXPIRATION_KEY);
+	return { token, expiration };
+};
 
 const AuthProvider = (props) => {
-	const [token, setToken] = useState();
-	const [expiration, setExpiration] = useState();
+	const tokenData = retrieveToken();
 
-	//autoLogin
-	useEffect(() => {}, []);
+	const [token, setToken] = useState(tokenData.token);
+	const [expiration, setExpiration] = useState(tokenData.expiration);
+	const history = useHistory();
+
+	const loginHandler = (token, expiration) => {
+		setToken(token);
+		setExpiration(expiration);
+		localStorage.setItem(LOCAL_TOKEN_KEY, token);
+		localStorage.setItem(LOCAL_TOKEN_EXPIRATION_KEY, expiration);
+		history.goBack();
+	};
+
+	const logoutHandler = () => {
+		setToken(null);
+		setExpiration(null);
+		localStorage.removeItem(LOCAL_TOKEN_KEY);
+		localStorage.removeItem(LOCAL_TOKEN_EXPIRATION_KEY);
+	};
 
 	//autoLogout
-	useEffect(() => {}, []);
+	useEffect(() => {
+		if (token && expiration) {
+			const currentTimeMili = new Date().getTime();
+			const expirationTimeMili = expiration * 1000;
+			const remaingTimeMili = expirationTimeMili - currentTimeMili;
 
-	const loginHandler = (token, expirationDate) => {};
-
-	const signInHandler = (token, expirationData) => {};
+			logoutTimer = setTimeout(logoutHandler, remaingTimeMili);
+		} else {
+			clearTimeout(logoutTimer);
+		}
+	}, [token, expiration]);
 
 	return (
-		<AuthContext.Provider value={{ login: loginHandler, signIn: signInHandler }}>
+		<AuthContext.Provider
+			value={{ login: loginHandler, logout: logoutHandler, isLoggedIn: !!token }}
+		>
 			{props.children}
 		</AuthContext.Provider>
 	);
--- frontend/src/store/auth-context.js
+++ frontend/src/store/auth-context.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const AuthContext = React.createContext({
-	login: (token, expirationDate) => {},
-	signIn: (token, expirationDate) => {},
+	login: (token, expiration) => {},
+	logout: () => {},
+	isLoggedIn: false,
 });
 
 export default AuthContext;
