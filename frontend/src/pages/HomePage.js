14798fa6a8a880d990aab28230a96dac386c283f
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-11-25 15:23:10 +0900

    fix: prevent homepage from resetting to top after exiting quote modal

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,8 @@
 import QuoteListContainer from "../components/Quotes/QuoteListContainer";
-import ScrollToTop from "../components/Utils/ScrollToTop";
 
 const MainPage = () => {
 	return (
 		<>
-			<ScrollToTop />
 			<QuoteListContainer />
 		</>
 	);
