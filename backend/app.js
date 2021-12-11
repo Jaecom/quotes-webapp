883f6e6c5c57e47d9c31ce181c929b26e2bf0731
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-12-11 15:06:30 +0900

    style: add missing semicolon

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ db.once("open", () => {
 db.on("error", console.error.bind(console, "connection error:"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
