79042486a3d18056b5cf0a6672797d3830fa6f85
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-12-20 00:32:11 +0900

    chore: remove unnecessary text

--- frontend/src/components/Auth/AuthBackground.js
+++ frontend/src/components/Auth/AuthBackground.js
@@ -3,7 +3,7 @@ import classes from "./AuthBackground.module.scss";
 const AuthBackground = (props) => {
 	return (
 		<div className={classes.wrapper}>
-			<div className={classes.background}>asd</div>
+			<div className={classes.background}></div>
 			<div className={classes.card}>{props.children}</div>
 		</div>
 	);
