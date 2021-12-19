ef7d593967226375892690806fefd35b9b1fc3f9
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-12-19 19:28:42 +0900

    feat: create login modal to enable user to login without redirecting to route

---
+++ frontend/src/components/Auth/LoginModal.js
@@ -0,0 +1,21 @@
+import Modal from "../UI/Modal";
+import Card from "../UI/Card";
+import LoginForm from "./LoginForm";
+
+import classes from "./LoginModal.module.scss";
+
+const LoginModal = (props) => {
+	return (
+		<Modal onClose={props.onClose}>
+			<div className={classes.wrapper}>
+				<Card>
+					<div className={classes.container}>
+						<LoginForm />
+					</div>
+				</Card>
+			</div>
+		</Modal>
+	);
+};
+
+export default LoginModal;
