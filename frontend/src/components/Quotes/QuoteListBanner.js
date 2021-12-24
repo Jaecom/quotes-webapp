7af8d64aee823699f3ccff45270fe2c87fb49080
Jaecom <50648539+Jaecom@users.noreply.github.com>
2021-12-24 16:40:44 +0900

    feat: create banner component that spans across quote list for recommendations, quote of day, signup, etc.

---
+++ frontend/src/components/Quotes/QuoteListBanner.js
@@ -0,0 +1,9 @@
+import classes from "./QuoteListBanner.module.scss";
+
+const QuoteListBanner = () => {
+	return <div className={classes.wrapper}>
+		<div>Quote of the Day</div>
+	</div>;
+};
+
+export default QuoteListBanner;
