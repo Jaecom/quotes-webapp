3992a88e8ac838193a68ed85c969c9282708a124
Jaecom <50648539+Jaecom@users.noreply.github.com>
2022-01-04 01:45:55 +0900

    feat: style banner component & add button to view quote detail

--- frontend/src/components/Quotes/QuoteListBanner.js
+++ frontend/src/components/Quotes/QuoteListBanner.js
@@ -1,9 +1,28 @@
 import classes from "./QuoteListBanner.module.scss";
+import Button from "../UI/Button";
+import { Link } from "react-router-dom";
+import useLinkStateBackground from "../../hooks/useLinkStateBackground";
 
-const QuoteListBanner = () => {
-	return <div className={classes.wrapper}>
-		<div>Quote of the Day</div>
-	</div>;
+const QuoteListBanner = (props) => {
+	const { quote } = props;
+	
+	const linkStateBackground = useLinkStateBackground();
+
+	return (
+		<div className={classes.wrapper}>
+			<div className={classes.container}>
+				<h3 className={classes.author}>{quote.author.name}</h3>
+				<p className={classes.paragraph}>{quote.quoteShort}</p>
+				<Link
+					className={classes.link}
+					to={{ pathname: `/quotes/${quote.id}`, state: linkStateBackground }}
+				>
+					<Button highlight>View Quote</Button>
+				</Link>
+				<p className={classes["double-quote"]}>&#8221;</p>
+			</div>
+		</div>
+	);
 };
 
 export default QuoteListBanner;
