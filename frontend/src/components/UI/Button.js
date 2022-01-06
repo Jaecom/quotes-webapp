1cc5c394698f736e086e5ba115b0f8380753bdf1
Jaecom <50648539+Jaecom@users.noreply.github.com>
2022-01-06 21:39:17 +0900

    fix: enable external classes to be added to Button component

--- frontend/src/components/UI/Button.js
+++ frontend/src/components/UI/Button.js
@@ -1,7 +1,8 @@
 import classes from "./Button.module.scss";
 
 const Button = (props) => {
-	const btnClass = classes.btn + (props.highlight && ` ${classes.highlight}`);
+	const btnClass =
+		classes.btn + (props.highlight && ` ${classes.highlight}`) + ` ${props.className}`;
 	return (
 		<button className={btnClass} onClick={props.onClick}>
 			{props.children}
