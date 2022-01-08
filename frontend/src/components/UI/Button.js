d1c012c31bca728d8dd4b4204d42fd97e65d264b
Jaecom <50648539+Jaecom@users.noreply.github.com>
2022-01-08 11:16:54 +0900

    refactor: concatenate Button classes using array (easier to understand)

--- frontend/src/components/UI/Button.js
+++ frontend/src/components/UI/Button.js
@@ -1,8 +1,15 @@
 import classes from "./Button.module.scss";
 
 const Button = (props) => {
-	const btnClass =
-		classes.btn + (props.highlight && ` ${classes.highlight}`) + ` ${props.className}`;
+	const btnClass = [
+		classes.btn,
+		props.highlight && classes.highlight,
+		props.outline && classes.outline,
+		props.className,
+	]
+		.filter((className) => className !== undefined)
+		.join(" ");
+
 	return (
 		<button className={btnClass} onClick={props.onClick}>
 			{props.children}
