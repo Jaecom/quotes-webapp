91f7b29002f959b51d760d930de151bc306697d3
Jaecom <50648539+Jaecom@users.noreply.github.com>
2022-01-08 11:32:14 +0900

    feat: set up backend for adding & removing quotes from collection

--- backend/controllers/collections-controllers.js
+++ backend/controllers/collections-controllers.js
@@ -4,11 +4,9 @@ const HttpError = require("../utils/HttpError");
 module.exports.index = async (req, res, next) => {
 	const { userId } = res.locals;
 
-	const user = await Users.findById(userId)
-		.populate("collections")
-		.catch((error) => {
-			next(new HttpError("Getting collection failed. Please try again later", 500));
-		});
+	const user = await Users.findById(userId).catch((error) => {
+		next(new HttpError("Getting collection failed. Please try again later", 500));
+	});
 
 	res.json(user.collections);
 };
@@ -21,7 +19,7 @@ module.exports.createCollection = async (req, res, next) => {
 		userId,
 		{
 			$push: {
-				collections: { name, description, content: [], isPrivate },
+				collections: { name, description, quotes: [], isPrivate },
 			},
 		},
 		{ new: true }
@@ -32,4 +30,48 @@ module.exports.createCollection = async (req, res, next) => {
 	const updatedCollection = updatedUser.collections[updatedUser.collections.length - 1];
 
 	res.json(updatedCollection);
+};
+
+module.exports.addQuoteToCollection = async (req, res, next) => {
+	const { userId } = res.locals;
+	const { collectionId } = req.params;
+	const { quoteId } = req.body;
+
+	const updatedUser = await Users.updateOne(
+		{
+			_id: userId,
+			"collections._id": collectionId,
+		},
+		{
+			$push: {
+				"collections.$.quotes": quoteId,
+			},
+		}
+	).catch((error) =>
+		next(new HttpError("Adding quote to collection failed. Please try again", 500))
+	);
+
+	res.json("Adding success");
+};
+
+module.exports.removeQuoteFromCollection = async (req, res, next) => {
+	const { userId } = res.locals;
+	const { collectionId } = req.params;
+	const { quoteId } = req.body;
+
+	const updatedUser = await Users.updateOne(
+		{
+			_id: userId,
+			"collections._id": collectionId,
+		},
+		{
+			$pull: {
+				"collections.$.quotes": quoteId,
+			},
+		}
+	).catch((error) =>
+		next(new HttpError("Deleting quote from collection failed. Please try again", 500))
+	);
+
+	res.json("Adding success");
 };
--- backend/routes/collections-routes.js
+++ backend/routes/collections-routes.js
@@ -8,4 +8,15 @@ router
 	.get("/", checkAuthentication, catchAsync(collectionController.index))
 	.post("/", checkAuthentication, catchAsync(collectionController.createCollection));
 
+router
+	.post(
+		"/:collectionId",
+		checkAuthentication,
+		catchAsync(collectionController.addQuoteToCollection)
+	)
+	.delete(
+		"/:collectionId",
+		checkAuthentication,
+		catchAsync(collectionController.removeQuoteFromCollection)
+	);
 module.exports = router;
