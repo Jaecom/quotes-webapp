e289b12a0028b888ba1624f26684d7386f50f269
Jaecom <50648539+Jaecom@users.noreply.github.com>
2022-03-31 14:36:02 +0900

    fix: prevent user's clicks from being blocked to open modal

--- frontend/src/components/Quotes/QuoteListBanner.tsx
+++ frontend/src/components/Quotes/QuoteListBanner.tsx
@@ -10,19 +10,19 @@ const QuoteListBanner = (props: { quote: Quote }) => {
 
 	return (
 		<div className={classes["grid-wrapper"]}>
-			<div className={classes.wrapper}>
+			<Link
+				className={classes.wrapper}
+				to={{ pathname: `/quotes/${quote._id}`, state: linkStateBackground }}
+			>
 				<div className={classes.container}>
-					<h3 className={`heading-3 ${classes.author}`}>{quote.author.name}</h3>
-					<p className="paragraph--medium">{quote.text.short}</p>
-					<Link
-						className={classes.link}
-						to={{ pathname: `/quotes/${quote._id}`, state: linkStateBackground }}
-					>
+					<div className={classes.content}>
+						<h3 className="heading-3">{quote.author.name}</h3>
+						<p className="paragraph--medium">{quote.text.short}</p>
 						<Button fill>View Quote</Button>
-					</Link>
+					</div>
 					<p className={`paragraph--big ${classes["double-quote"]}`}>&#8221;</p>
 				</div>
-			</div>
+			</Link>
 		</div>
 	);
 };
